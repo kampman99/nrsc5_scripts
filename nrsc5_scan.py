ddcecc9a008a4bc676161e7577da5a1fba010127
Bill Kampman <bill@kampman.org>
2022-10-27 18:04:04 -0400

    Modified text and file output. Track start and end freq. Capture SIG Services.

--- nrsc5_scan.py
+++ nrsc5_scan.py
@@ -3,14 +3,18 @@ from datetime import datetime
 
 def main():
     
-    freq = 87.9
+    start_freq = 87.9
     count_found = 0
     timeout = 15
     
+    freq = start_freq
+
     while freq <= 108.1:
 
         # Call nrsc5 and see if it can play program 0 on this frequency
         stderr = EasyProcess(f'nrsc5 {freq} 0').call(timeout = timeout).stderr
+
+        end_freq = freq
 
         if 'Synchronized' in stderr:
             
@@ -22,6 +26,8 @@ def main():
             message = ''
             station_location = ''
             station_name_only = '----'
+            sig_services = []
+            text = ''
 
             output_list = stderr.splitlines()
             for line in output_list:
@@ -41,20 +47,35 @@ def main():
                     message = line.split(' ', 1)[1]
                 if 'Station location' in line:
                     station_location = line.split(' ', 1)[1]
+                if 'SIG Service:' in line:
+                    sig_services.append(line.split(' ', 1)[1])
                     
-            print(f'    Frequency: {freq} MHz')
-            print(f'    {station_name}') if station_name else print('    No station name')
-            print(f'    {station_location}') if station_location else print('    No station location')
-            print(f'    {message}') if message else print('    No message')
-            print(f'    {audio_prog_0}') if audio_prog_0 else print('    No audio program 0')
-            print(f'    {audio_prog_1}') if audio_prog_1 else print('    No audio program 1')
-            print(f'    {audio_prog_2}') if audio_prog_2 else print('    No audio program 2')
-            print(f'    {audio_prog_3}') if audio_prog_3 else print('    No audio program 3')
+            text += f'    Frequency: {freq} MHz'
+            text += f'    {station_name}' if station_name else '    No station name'
+            text += f'    {station_location}' if station_location else '    No station location'
+            text += f'    {message}' if message else '    No message'
+            if audio_prog_0:
+                text += f'    {audio_prog_0}'
+            if audio_prog_1:
+                text += f'    {audio_prog_1}'
+            if audio_prog_2:
+                text += f'    {audio_prog_2}'
+            if audio_prog_3:
+                text += f'    {audio_prog_3}'
+            if sig_services:
+                for service in sig_services:
+                    text += f'    {service}\n'
+
+            if text:
+                print(text)
+
             print()
 
             date_time = datetime.now().strftime("%Y-%m-%d %H%M%S")
             with open(f'{freq} {station_name_only} {date_time}.txt', 'w') as f:
                 f.write(stderr)
+                f.write('\n')
+                f.write(text)
                 f.close()
             
             count_found += 1
@@ -62,7 +83,7 @@ def main():
         freq += .2
         freq = round(freq, 1)
     
-    print(f'Scan complete. {count_found} HD stations found.')
+    print(f'Scan {start_freq} - {end_freq} complete. {count_found} HD stations found.')
 
 
 if __name__ == "__main__":
